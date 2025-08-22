cf0c53c92c2dc8539924b5c0f427175b8a8dd118
garciadecastro <carlosgarciadecastro@gmail.com>
2025-08-22 17:17:25 -0300

    segundo servidor arranca2

--- clase2/server.js
+++ clase2/server.js
@@ -1,7 +1,8 @@
 const http = require("http")
 
 const server = http.createServer(function(request, response){
-  
+    console.log("Route que me envió el cliente: ", request.url);
+  response.end("Puedo enviar un mensaje, pero no usar tildes á é í ó ú");
 })
 
 server.listen(2025, () => {
