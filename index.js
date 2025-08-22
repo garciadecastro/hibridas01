75a0e1c4128e0eca331d2c57fcec19580bacfb0b
garciadecastro <carlosgarciadecastro@gmail.com>
2025-08-22 16:53:48 -0300

    servidor1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hola desde node");
+const http = require("http");
+
+//ya tengo el protocolo, commonjs
+
+//import http from "http" requiere más configuración
+
+//1 NECESITAMOS FUNCIÓN DE CALL BACK
+
+http.createServer( function(request, response){
+    console.log("Hola desde el servidor");
+}).listen(2025);
